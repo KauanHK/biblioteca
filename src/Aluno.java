ee15f1198d7df8bad8f918fa1f2e35c4fa0fbd89
KauanHK <kauankaestner06@gmail.com>
2024-12-03 09:25:54 -0300

    Correção do método solicitarEmprestimo

--- src/Aluno.java
+++ src/Aluno.java
@@ -25,22 +25,27 @@ public class Aluno extends Pessoa{
     public void solicitarEmprestimo(Livro livro) {
 
         if (!livro.isDisponivel()) {
-            System.out.println("Livro não disponível para empréstimo");
+            System.out.println("Livro " + livro.getTitulo() + " estará disponível apenas a partir do dia " + livro.getDataDevolucao());
+            return;
         }
         
-        if (getLimiteEmprestimo() <= 0) {
-            System.out.println("Limite de empréstimos excedido");
+        if (livrosEmprestados.size() >=  limiteEmprestimo) {
+            System.out.println("Empréstimo não permitido:");
+            System.out.println("O aluno " + nome + " já possui " + livrosEmprestados.size() + " livros emprestados (limite: " + limiteEmprestimo + ")");
+            return;
         }
         
-        livro.setDisponivel(false);
+        livro.emprestar();
         emprestaLivro(livro);
         
         setLimiteEmprestimo(getLimiteEmprestimo() - 1);
-        
-        System.out.println("Empréstimo realizado para o aluno " + getNome() +
-                     " - Curso: " + getCurso() + 
-                     " - Período: " + getPeriodo() + 
-                     " - Livro: " + livro.getTitulo());
+
+        System.out.println("================================================");
+        System.out.println("Empréstimo realizado para o aluno " + getNome());
+        System.out.println(" - Curso: " + getCurso()) ;
+        System.out.println(" - Período: " + getPeriodo());
+        System.out.println(" - Livro: " + livro.getTitulo());
+        System.out.println("================================================");
 
     }
 
